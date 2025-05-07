7525ddc354bdeb6edcb85afb5734037d5d439e90
mhuzaifa24 <bscs23139@itu.edu.pk>
2025-05-07 21:30:16 +0500

    Add Sidenav.jsx with sidebar navigation links

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Sidenav.module.css';
+
+function Sidenav() {
+  return (
+    <aside className={styles.sidenav}>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/education">Education</a></li>
+        <li><a href="/projects">Projects</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidenav;
